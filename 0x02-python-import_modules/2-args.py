a3a4581e57b448556e20037c5bf84e7daf0a2d6f
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-01-04 17:48:00 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
-if __name__ = "__main__":
-    i = 1
-    args = len(argv) - 1
-    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
-    print(f"{'.' if args == 0 else ':'}")
-    while (i <= args):
-        print(f"{i}: {argv[i]}")
-        i += 1
+
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1

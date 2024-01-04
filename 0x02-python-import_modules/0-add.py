559402f3b102c441beaa89b930e54c7044154cc5
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-01-04 17:46:36 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, a + b))
+
+from add_0 import add
 
 if __name__ == "__main__":
-    print("Running...")
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

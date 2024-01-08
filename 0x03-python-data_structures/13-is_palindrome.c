302452f368ec64eae8d73f2b227407b50bbe6f52
sarahwonago <sarahshaaleh@gmail.com>
2024-01-08 22:45:53 +0300

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+
+void reverse(listint_t **head);
+int compare_lists(listint_t *head, listint_t *middle, int len);
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * 
+ *@head

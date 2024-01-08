45132163eaeb00785b278217975f7177dc44e70c
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-01-08 22:38:25 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string[:]:
-        new_string = my_string.translate({ord("c"): None})
-        sec_string =new_string.translate({ord("C"): None})
-        return sec_string
-    return my_string
-    
+    new_str = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_str += i
+    return (new_str)

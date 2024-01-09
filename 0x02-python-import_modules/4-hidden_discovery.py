cd59478f652c8299d0aba5ae86d3c00d34931325
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-01-09 21:54:22 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   from hidden_4 import *
-   def_names = dir()
-   for i in range(0, len(def_names)):
+    from hidden_4 import *
+    def_names = dir()
+    for i in range(0, len(def_names)):
         if def_names[i][:2] != '__':
             print("{:s}".format(def_names[i]))

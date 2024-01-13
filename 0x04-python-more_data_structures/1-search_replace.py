b4999ea01494fd1e324fd4eaabee9c324561aa1d
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-01-13 22:18:22 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    index = new_list.index(search)
-    new_list[index] = replace
-    return new_list
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy

665e20f2d95ac6624ebaac02448f2fec4de03e37
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-02-07 16:01:38 +0300

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,5 +7,5 @@ class MyList(list):
 	"""implements sorted printing """
 	
 	def print_sorted(self):
-		"""prints list, but sorted in ascending sort"""
+		"""prints list, sorted in ascending sort"""
 		print(sorted(self))

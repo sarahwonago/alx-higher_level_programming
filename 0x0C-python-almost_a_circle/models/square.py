7a38db277148164ca89624450ba772c5106fb810
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-02-12 17:46:02 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,7 +27,46 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+     def update(self, *args, **kwargs):
+        """Update Square.
+        Args:
+            *args (int): New attribute values.
+                - first argument represent the id attribute
+                - second argument represents size attribute
+                - third argument represents x attribute
+                - forth argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) is not 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+  
     def __str__(self):
         """Return a string representation of the Square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-

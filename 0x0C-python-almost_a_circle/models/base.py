dc56ea6f766f5a92924f5aa3301a42d9861832aa
sarahwonago <sarahshaaleh@gmail.com>
2024-02-12 15:51:40 +0300

    created base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Defines class: Base"""
+
+
+class Base:
+    """ Base class
+    The  class Base  will be the “base” of all other classes in this project.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Base objects.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+
+        """Initializing a new Base.
+        Args:
+            id (int): New Base Id.
+        """
+
+        if (id is not None):
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
+

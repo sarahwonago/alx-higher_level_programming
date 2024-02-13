54286b292681bcd50cb6b99a55624891872541cc
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-02-13 14:37:21 +0300

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -18,57 +18,24 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square_3.x, 2)
         self.assertEqual(square_3.y, 3)
 
-        # Add more tests for different scenarios...
-
     def test_invalid_square_creation(self):
         # Test Square("1")
         with self.assertRaises(ValueError):
             Square("1")
-
-        # Test Square(1, "2")
-        with self.assertRaises(ValueError):
-            Square(1, "2")
-
-        # Test Square(1, 2, "3")
-        with self.assertRaises(ValueError):
-            Square(1, 2, "3")
-
-        # Add more tests for different scenarios...
 
     def test_str_method(self):
         # Test __str__() for Square
         square = Square(3, 4, 5)
         self.assertEqual(str(square), "[Square] (0) 4/5 - 3")
 
-    def test_to_dictionary_method(self):
-        # Test to_dictionary() for Square
-        square = Square(3, 4, 5, 1)
-        square_dict = {'id': 1, 'size': 3, 'x': 4, 'y': 5}
-        self.assertEqual(square.to_dictionary(), square_dict)
-
-    def test_update_method(self):
-        # Test update() for Square
-        square = Square(3, 4, 5, 1)
-        square.update(2, 6, 7, 8)
-        self.assertEqual(square.size, 6)
-        self.assertEqual(square.x, 7)
-        self.assertEqual(square.y, 8)
-        self.assertEqual(square.id, 2)
-
-    def test_create_method(self):
-        # Test create() for Square
-        square_dict = {'id': 1, 'size': 3, 'x': 4, 'y': 5}
-        square = Square.create(**square_dict)
-        self.assertEqual(square.to_dictionary(), square_dict)
-
     def test_save_and_load_from_file_methods(self):
         # Test save_to_file() and load_from_file() for Square
         filename = "test_square.json"
         square = Square(3, 4, 5, 1)
-        
+
         # Save to file
         Square.save_to_file([square], filename)
-        
+
         # Load from file
         loaded_squares = Square.load_from_file(filename)
         self.assertEqual(len(loaded_squares), 1)
@@ -76,4 +43,3 @@ class TestSquare(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-

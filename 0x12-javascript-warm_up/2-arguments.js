da8d8b2ed8777196ce3e7988e2e882f8e7de260d
sarahwonago <sarahshaaleh@gmail.com>
2024-04-08 19:28:39 +0300

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const numOfArgs = process.argv.length - 2;
+if (numOfArgs === 0) {
+  console.log('No argument');
+} else if (numOfArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

ba4e2fe0c132f34cdaffc7278f4613ff25457741
sarahwonago <sarahshaaleh@gmail.com>
2024-04-13 10:06:19 +0300

    MySqldb SqlAlchemy

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Lists all cities from the hbtn_0e_4_usa database.
+"""
+
+import sys
+import MySQLdb
+
+def list_cities(username, password, database):
+    """
+    Lists all cities from the hbtn_0e_4_usa database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
+    """
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+        cursor = db.cursor()
+
+        query = """
+            SELECT cities.id, cities.name, states.name
+            FROM cities
+            INNER JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC
+        """
+        cursor.execute(query)
+
+        cities = cursor.fetchall()
+        for city in cities:
+            print(city)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+    finally:
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_cities(username, password, database)

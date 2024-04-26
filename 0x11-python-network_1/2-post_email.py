adb0269a10f296d4c99190dab4793f0cc130c80a
sarahwonago <sarahshaaleh@gmail.com>
2024-04-26 15:12:40 +0300

    post email error code

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Python script that takes a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {'email': email}
+
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status() 
+
+        print("Your email is: {}".format(response.text))
+
+    except requests.exceptions.HTTPError as errh:
+        print(f'HTTP Error: {errh}')
+    except requests.exceptions.ConnectionError as errc:
+        print(f'Error Connecting: {errc}')
+    except requests.exceptions.Timeout as errt:
+        print(f'Timeout Error: {errt}')
+    except requests.exceptions.RequestException as err:
+        print(f'Request Exception: {err}')
